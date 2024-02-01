d597f20af1b8f596b418c1277ebb40354c1c3356
4arters <158380095+4arters@users.noreply.github.com>
2024-02-01 09:00:39 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='w-full h-20 bg-slate-200 text-red-600 flex flex-row-reverse'>tis isno header</div>
+  )
+}
